27c9b71b3ec72e2a81e904adbb19185f052454a6
Pierre Gaimard <pierre@gaimard-family.fr>
2021-02-08 16:13:34 +0100

    Configure AppAuthenticator class

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -71,7 +71,7 @@ class AppAuthenticator extends AbstractFormLoginAuthenticator implements Passwor
 
         if (!$user) {
             // fail authentication with a custom error
-            throw new CustomUserMessageAuthenticationException('Username could not be found.');
+            throw new CustomUserMessageAuthenticationException('Identifiant introuvable.');
         }
 
         return $user;
@@ -96,8 +96,7 @@ class AppAuthenticator extends AbstractFormLoginAuthenticator implements Passwor
             return new RedirectResponse($targetPath);
         }
 
-        // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->urlGenerator->generate('home'));
     }
 
     protected function getLoginUrl()
