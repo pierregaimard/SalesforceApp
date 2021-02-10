bd6354425baf9f7bcbbc27449784959bfdb8a4a3
Pierre Gaimard <pierre@gaimard-family.fr>
2021-02-10 09:05:17 +0100

    Add AuthenticationFailureException

---
+++ src/Salesforce/AuthenticationFailureException.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Salesforce;
+
+use Throwable;
+
+class AuthenticationFailureException extends \Exception
+{
+    /**
+     * @param string         $message
+     * @param int            $code
+     * @param Throwable|null $previous
+     */
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
